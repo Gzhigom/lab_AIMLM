fbd0518380b43b6446c186cb16c03621b42b88d5
Gzhigom <gzhsh8465@gmail.com>
2026-04-01 21:47:38 +0300

    test_changes

--- AILab/1labAI.py
+++ AILab/1labAI.py
@@ -1,9 +1,7 @@
-
 import pandas as pd
-
+from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv(r"C:\Global_Cybersecurity_Threats_2015-2024.csv")
-
 print("Информация о датасете:")
 print(df.info())
 print("\n")
@@ -16,29 +14,21 @@ print("Названия столбцов:")
 print(df.columns.tolist())
 print("\n")
 
-#############################################################################################
-#                                         ПРОПУСКИ
-#############################################################################################
-
-
-# Поиск
+columns_to_drop = ['Year', 'Country', 'Resolution Time (in Hours)']
+df = df.drop(columns=[col for col in columns_to_drop if col in df.columns])
 
 print("Количество пропущенных значений по столбцам:")
 print(df.isnull().sum())
 print("\n")
 
-
-# Заполнение
-
-
-# Числовые столбцы (сред)
+#Числовые столбцы (сред)
 numeric_cols = df.select_dtypes(include='number').columns
 
 for col in numeric_cols:
     mean_value = df[col].mean()
     df[col] = df[col].fillna(mean_value)
 
-# Категориальные столбцы (мода)
+#Категориальные столбцы (мода)
 categorical_cols = df.select_dtypes(exclude='number').columns
 
 for col in categorical_cols:
@@ -49,38 +39,20 @@ print("Пропуски после заполнения:")
 print(df.isnull().sum())
 print("\n")
 
-#############################################################################################
-#                             Нормализация (Min-Max)
-#############################################################################################
-
-for col in numeric_cols:
-    min_value = df[col].min()
-    max_value = df[col].max()
-
-    #  деление на 0
-    if max_value != min_value:
-        df[col] = (df[col] - min_value) / (max_value - min_value)
-    else:
-        df[col] = 0
+#Нормализация (Min-Max)
+scaler = MinMaxScaler()
+df[numeric_cols] = scaler.fit_transform(df[numeric_cols])
 
 print("Данные после нормализации:")
 print(df.head())
 print("\n")
 
-#############################################################################################
-#                                     One-Hot Encoding
-#############################################################################################
-
+#One-Hot Encoding
 df = pd.get_dummies(df, columns=categorical_cols, drop_first=True)
 
 print("Данные после OHE-кодирования:")
 print(df.head())
 print("\n")
 
-#############################################################################################
-# 7. Сохранение обработанного датасета
-#############################################################################################
-
+#Сохранение
 df.to_csv("processed_cybersecurity_dataset.csv", index=False)
-
-print("Обработка завершена. Файл сохранён как processed_cybersecurity_dataset.csv")
--- AILab/2lab.py
+++ AILab/2lab.py
@@ -3,81 +3,52 @@ import numpy as np
 
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import train_test_split
-
 # Метрики
-from sklearn.metrics import (
-    mean_squared_error,
-    mean_absolute_error,
-    accuracy_score,
-    classification_report
-)
-
+from sklearn.metrics import mean_squared_error, mean_absolute_error,accuracy_score,classification_report,r2_score
 df = pd.read_csv('processed_cybersecurity_dataset.csv')
 
-print("Размер датасета:", df.shape)
 print(df.head())
-
-##################################
 # РЕГРЕССИЯ
-##################################
-
 print("\n РЕГРЕССИЯ")
-
 # Целевая переменная
 y_reg = df['Financial Loss (in Million $)']
-
 # Признаки
 X_reg = df.drop(columns=['Financial Loss (in Million $)'])
-
 # Разделение
 X_train_r, X_test_r, y_train_r, y_test_r = train_test_split(
     X_reg, y_reg, test_size=0.2, random_state=19
 )
-
 # Модель
 reg_model = LinearRegression()
 reg_model.fit(X_train_r, y_train_r)
-
 # Предсказание
 y_pred_r = reg_model.predict(X_test_r)
-
 # Оценка
 mse = mean_squared_error(y_test_r, y_pred_r)
 rmse = np.sqrt(mse)
 mae = mean_absolute_error(y_test_r, y_pred_r)
-
-print(f"MSE: {mse:.4f}")
-print(f"RMSE: {rmse:.4f}")
-print(f"MAE: {mae:.4f}")
-
-##################################
+r2=r2_score(y_test_r, y_pred_r)
+print(f"MSE: ",mse)
+print(f"RMSE: ", rmse)
+print(f"MAE: ", mae)
+print(f"R2: ", r2)
 # КЛАССИФИКАЦИЯ
-##################################
-
 print("\n КЛАССИФИКАЦИЯ")
-
 # Целевая переменная (бинарная)
 y_clf = df['Defense Mechanism Used_VPN'].astype(int)
-
 # Признаки
 X_clf = df.drop(columns=['Defense Mechanism Used_VPN'])
-
 # Разделение
 X_train_c, X_test_c, y_train_c, y_test_c = train_test_split(
     X_clf, y_clf, test_size=0.2, random_state=19
 )
-
 # Модель
 clf_model = LogisticRegression(max_iter=1000,class_weight='balanced')
 clf_model.fit(X_train_c, y_train_c)
-
-# Предсказание
+# Предсазание
 y_pred_c = clf_model.predict(X_test_c)
-
 # Оценка
 accuracy = accuracy_score(y_test_c, y_pred_c)
-
 print(f"Accuracy: {accuracy:.4f}")
-
 print("\nClassification Report:")
 print(classification_report(y_test_c, y_pred_c))
